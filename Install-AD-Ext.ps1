34d5be95004375ef51ca10c91f3e23243554c672
isshukla <47317576+isshukla@users.noreply.github.com>
2019-04-24 19:37:57 +0530

    Update Install-AD-Ext.ps1

--- Install-AD-Ext.ps1
+++ Install-AD-Ext.ps1
@@ -6,17 +6,20 @@ Install-windowsfeature AD-Domain-Services
 $password = "Password12345"
 $SecurePswd = ConvertTo-SecureString $password -AsPlainText -Force
 
+$DP = “C:\Windows\NTDS”
+$syspt = “C:\Windows\SYSVOL”
+
 Install-ADDSForest `
 -CreateDnsDelegation:$false `
--DatabasePath “C:\Windows\NTDS” `
+-DatabasePath $DP `
 -DomainMode “Default” `
 -DomainName “testdomain.com” `
 -DomainNetbiosName “TESTDOMAIN” `
 -ForestMode “Default” `
 -InstallDns:$true `
--LogPath “C:\Windows\NTDS” `
+-LogPath $DP `
 -NoRebootOnCompletion:$false `
--SysvolPath “C:\Windows\SYSVOL” `
+-SysvolPath $syspt `
 -Force:$true -SafeModeAdministratorPassword $SecurePswd
 
 
