9bd80f7f5be82e699517effa61b91fa2450f6b9c
isshukla <47317576+isshukla@users.noreply.github.com>
2019-04-25 11:01:08 +0530

    Update join-domain.ps1

--- join-domain.ps1
+++ join-domain.ps1
@@ -1,7 +1,7 @@
    $VMLocalAdminUser = 'isshukla'
    $password = 'Password12345'
    $VMLocalAdminSecurePassword = ConvertTo-SecureString $password -AsPlainText -Force
-   $cred = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword)
+   $cred = New-Object System.Management.Automation.PSCredential($VMLocalAdminUser, $VMLocalAdminSecurePassword)
    Add-Computer -DomainName testdomain.com -Credential $cred -Restart -Force
    
    
