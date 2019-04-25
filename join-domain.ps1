1940b46ba54005e91cca28f5d4134231199c0dc6
isshukla <47317576+isshukla@users.noreply.github.com>
2019-04-25 12:36:27 +0530

    Update join-domain.ps1

--- join-domain.ps1
+++ join-domain.ps1
@@ -2,6 +2,6 @@
    $password = 'Password12345'
    $VMLocalAdminSecurePassword = ConvertTo-SecureString $password -AsPlainText -Force
    $cred = New-Object PSCredential($VMLocalAdminUser, $VMLocalAdminSecurePassword)
-   Add-Computer -DomainName testdomain.com -Credential $cred -Restart -Force
+   Add-Computer -DomainName testdomain.com -Options UnsecuredJoin,PasswordPass -Credential $cred -Restart -Force
    
    
