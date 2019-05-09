ba6230b6d69e1172f9047f89e3bd4a9c515e08be
isshukla <47317576+isshukla@users.noreply.github.com>
2019-05-09 10:16:08 +0530

    Update Cluster1.ps1

--- Cluster1.ps1
+++ Cluster1.ps1
@@ -1,1 +1,13 @@
+#Install Cluster 
+$nodes = ("SQL1","SQL2","SQL3")
+Invoke-Command  $nodes {Install-WindowsFeature Failover-Clustering -IncludeAllSubFeature -IncludeManagementTools}
+
+
+$user = "testdomain\isshukla" 
+$pwd1 = "Password12345"
+$pwd = ($pwd1 | ConvertTo-SecureString)
+$Credential = New-Object System.Management.Automation.PSCredential $user, $pwd
+Start-Process powershell.exe -Credential $Credential 
+
+#Create Cluster
 New-Cluster -Name AzClu -Node ("SQL1","SQL2","SQL3") –StaticAddress 172.30.0.11 -NoStorage
